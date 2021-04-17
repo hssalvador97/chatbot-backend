e0c60d07c785435d7362bed7c7836d7e7cc2041b
Salvador Hernandez <shernandez@centraldealarmas.com.mx>
2021-04-16 19:04:47 -0500

    Config

--- src/controladores/chat_rest.js
+++ src/controladores/chat_rest.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const app = express();
 const { GESTOCAR_T_USUARIO } = require('../database/models/index');
-app.get("/", express.json(), (req, res) => {
+// app.get("/", express.json(), (req, res) => {
 
-    try {
-        GESTOCAR_T_USUARIO.findAll().then(async Consultas => {
-            res.json({ ok: true, status: 200, consultas: Consultas });
-        });
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ ok: false, status: 500, mensaje: error });
-    }
-});
+//     try {
+//         GESTOCAR_T_USUARIO.findAll().then(async Consultas => {
+//             res.json({ ok: true, status: 200, consultas: Consultas });
+//         });
+//     } catch (error) {
+//         console.log(error)
+//         res.status(500).json({ ok: false, status: 500, mensaje: error });
+//     }
+// });
 
 
 app.post("/conversacion", (req, res) => {
--- src/server.js
+++ src/server.js
@@ -1,25 +1,28 @@
-'use strict';
 require('./config/config');
 const express = require("express");
 const bodyParser = require('body-parser');
+const path = require('path');
 const request = require('request');
 const cors = require('cors');
 const app = express();
+
+require('dotenv').config();
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(require('./controladores/indexControlador'));
 
 const { sequelize } = require('./database/models/index');
-
-
-
-// const http = require('http');
-// let server = http.createServer(app);
-
 const server = require('http').createServer(app);
 module.exports.io = require('socket.io')(server);
 
 require('./controladores/sockets_chat');
+
+
+const publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath));
+
+
 server.listen(process.env.PORT, () => {
     console.log(process.env.PORT);
     sequelize.authenticate().then(() => {
@@ -28,25 +31,3 @@ server.listen(process.env.PORT, () => {
         console.log("Error en la conexion de la base de datos", error);
     });
 });
-
-
-
-// SOCKETS
-// const SocketIO = require("socket.io");
-
-// let ios = SocketIO(server);
-// module.exports.io = ios;
-
-// io.on('connection', (socket) => {
-//     console.log("USUARIO CONECTADO", socket.id);
-
-//     socket.on('chat:mensaje', (data) => {
-
-//         // ENVIAR A TODOS INCLUYENDOME A MI 
-//         io.sockets.emit('chat:nuevomensaje', data);
-//         console.log(data);
-//     });
-
-
-
-// });
