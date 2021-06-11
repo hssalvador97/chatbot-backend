7bc3fe583b24e50bef0d5b2c04cc083779f666c8
Salvador Hernandez <shernandez@centraldealarmas.com.mx>
2021-06-10 21:07:42 -0500

    Usuario actualizado

--- src/database/config/keys.js
+++ src/database/config/keys.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-    username: process.env.DB_USER,
+    username: "sisdgmx",
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
